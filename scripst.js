5e36400d708be6dc79fd162222f7a24a03a43f2d
John Rodrigues <johnrodrigues97106@gmail.com>
2021-10-28 16:40:52 -0300

    2 Commit

---
+++ scripst.js
@@ -0,0 +1,16 @@
+const openModal = document.getElementById('openModal')
+
+const modalWrapper = document.querySelector('.modal-wrapper')
+
+openModal.onclick = function () {
+    modalWrapper.classList.remove('invisible')
+}
+
+document.addEventListener('keydown',function(event) {
+    const isEscKey = event.key === 'Escape'
+
+    if(isEscKey){
+        modalWrapper.classList.add('invisible')
+
+    }
+})
